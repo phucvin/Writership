86a6a5aef9d146f727fafd84af8c4638578d5104
phucvin <phucvin2@gmail.com>
2018-09-01 02:23:52 +0700

    start draft

---
+++ Assets/Writership/Draft.cs
@@ -0,0 +1,31 @@
+﻿namespace Writership
+{
+    public class Engine
+    {
+        public El<T> El<T>(T value)
+        {
+            return new El<T>(this, value);
+        }
+
+        public Op<T> Op<T>()
+        {
+            return new Op<T>(this);
+        }
+    }
+
+    public class El<T>
+    {
+        public El(Engine engine, T value)
+        {
+
+        }
+    }
+
+    public class Op<T>
+    {
+        public Op(Engine engine)
+        {
+
+        }
+    }
+}
