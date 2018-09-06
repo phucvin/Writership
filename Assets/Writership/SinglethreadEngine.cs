b354110d55f5785e13477d4550d66369b5879ea6
phucvin <phucvin2@gmail.com>
2018-09-06 07:57:46 +0700

    SinglethreadEngine

--- Assets/Writership/SinglethreadEngine.cs
+++ Assets/Writership/SinglethreadEngine.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
 
 namespace Writership
 {
-    public class MultithreadEngine : IEngine, IDisposable
+    public class SinglethreadEngine : IEngine, IDisposable
     {
         private class Dirty
         {
@@ -15,37 +14,12 @@ namespace Writership
         private readonly List<Dirty>[] dirties;
         private readonly Dictionary<object, List<Action>>[] listeners;
 
-#if !WRITERSHIP_NO_COMPUTE_THREAD
-        private readonly int mainThreadId;
-        private bool isComputeDone;
-        private Thread computeThread;
-        private AutoResetEvent computeSignal;
-#endif
-
-        public MultithreadEngine()
+        public SinglethreadEngine()
         {
-#if !WRITERSHIP_NO_COMPUTE_THREAD
-            TotalCells = 3;
-#else
             TotalCells = 2;
-#endif
+
             dirties = new List<Dirty>[TotalCells];
             listeners = new Dictionary<object, List<Action>>[TotalCells];
-
-#if !WRITERSHIP_NO_COMPUTE_THREAD
-            mainThreadId = Thread.CurrentThread.ManagedThreadId;
-            isComputeDone = false;
-            if (TotalCells == 3)
-            {
-                computeThread = new Thread(Compute);
-                computeThread.Start();
-            }
-            else
-            {
-                computeThread = null;
-            }
-            computeSignal = new AutoResetEvent(false);
-#endif
 
             for (int i = 0, n = TotalCells; i < n; ++i)
             {
@@ -56,13 +30,6 @@ namespace Writership
 
         public void Dispose()
         {
-#if !WRITERSHIP_NO_COMPUTE_THREAD
-            if (computeThread != null)
-            {
-                computeThread.Abort();
-                computeThread = null;
-            }
-#endif
         }
 
         public int TotalCells { get; private set; }
@@ -74,12 +41,7 @@ namespace Writership
         {
             get
             {
-#if !WRITERSHIP_NO_COMPUTE_THREAD
-                if (Thread.CurrentThread.ManagedThreadId == mainThreadId) return ReadCellIndex;
-                else return ComputeCellIndex;
-#else
                 return ReadCellIndex;
-#endif
             }
         }
 
@@ -118,8 +80,6 @@ namespace Writership
 
         public void UnregisterListener(int at, object[] targets, Action job)
         {
-            // TODO Lock or use thread-safe collections
-            // to avoid multiple threads register at same time
             var listeners = this.listeners[at];
             for (int i = 0, n = targets.Length; i < n; ++i)
             {
@@ -136,25 +96,8 @@ namespace Writership
 
         public void Update()
         {
-#if !WRITERSHIP_NO_COMPUTE_THREAD
-            while (!isComputeDone)
-            {
-                Thread.Sleep(1);
-            }
-            CopyDirties(ComputeCellIndex, ReadCellIndex);
-            dirties[ComputeCellIndex].Clear();
-
-            CopyCells(WriteCellIndex, ReadCellIndex);
-            CopyDirties(ReadCellIndex, ComputeCellIndex);
-            isComputeDone = false;
-            computeSignal.Set();
-
-            Notify(ReadCellIndex);
-            dirties[ReadCellIndex].Clear();
-#else
             Process(ReadCellIndex);
             dirties[ReadCellIndex].Clear();
-#endif
         }
 
         private void CopyCells(int from, int to)
@@ -219,51 +162,8 @@ namespace Writership
             }
         }
 
-#if !WRITERSHIP_NO_COMPUTE_THREAD
-        private void Compute()
-        {
-            while (true)
-            {
-                Process(1);
-                isComputeDone = true;
-                computeSignal.WaitOne();
-            }
-        }
-
-        private void CopyDirties(int from, int to)
-        {
-            var fromDirties = dirties[from];
-            var toDirties = dirties[to];
-            for (int i = 0, n = fromDirties.Count; i < n; ++i)
-            {
-                var dirty = fromDirties[i];
-                if (dirty.Phase < 2) throw new NotImplementedException();
-                else if (dirty.Phase > 10) continue;
-
-                dirty.Inner.CopyCell(from, to);
-
-                var existing = toDirties.Find(it => ReferenceEquals(it.Inner, dirty.Inner));
-                if (existing == null)
-                {
-                    existing = new Dirty
-                    {
-                        Phase = 11,
-                        Inner = dirty.Inner,
-                    };
-                    toDirties.Add(existing);
-                }
-                else
-                {
-                    throw new NotImplementedException();
-                }
-            }
-        }
-#endif
-
         private void RegisterListener(int at, object[] targets, Action job)
         {
-            // TODO Lock or use thread-safe collections
-            // to avoid multiple threads register at same time
             var listeners = this.listeners[at];
             for (int i = 0, n = targets.Length; i < n; ++i)
             {
