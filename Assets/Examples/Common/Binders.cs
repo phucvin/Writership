fda5a1fb6a445341151bc7e4a5f90d4f65ca2cba
phucvin <phucvin2@gmail.com>
2018-09-07 03:05:36 +0700

    warn on something is not binded

--- Assets/Examples/Common/Binders.cs
+++ Assets/Examples/Common/Binders.cs
@@ -11,12 +11,14 @@ namespace Examples.Common
     {
         public static IDisposable Label<T>(IEngine engine, Text dst, IEl<T> src, Func<T, string> converter)
         {
+            if (!dst) return NotBinded.Instance;
             return engine.RegisterListener(new object[] { src }, () =>
                 dst.text = converter(src.Read()));
         }
 
         public static IDisposable ButtonClick<T>(IEngine engine, Button src, IOp<T> dst, Func<T> valueGetter)
         {
+            if (!src) return NotBinded.Instance;
             UnityAction action = () => dst.Fire(valueGetter());
             src.onClick.AddListener(action);
             return new RemoveOnClickListener(src, action);
@@ -24,6 +26,7 @@ namespace Examples.Common
 
         public static IDisposable List<T>(IEngine engine, Transform dst, Map prefab, ILi<T> src, Func<Map, T, IDisposable> itemBinder)
         {
+            if (!dst || !prefab) return NotBinded.Instance;
             var createdGameObjects = new List<GameObject>();
             var createdCd = new CompositeDisposable();
             return engine.RegisterListener(new object[] { src }, () =>
@@ -44,6 +47,26 @@ namespace Examples.Common
                     createdGameObjects.Add(itemMap.gameObject);
                 }
             });
+        }
+
+        private class NotBinded : IDisposable
+        {
+#if DEBUG
+            private static readonly NotBinded instance = new NotBinded();
+
+            public static NotBinded Instance
+            {
+                get
+                {
+                    Debug.LogWarning("Something is not binded, see trace for details");
+                    return instance;
+                }
+            }
+#else
+        public static readonly NoOpDisposable Instance = new NoOpDisposable();
+#endif
+
+            public void Dispose() { }
         }
     }
 }
