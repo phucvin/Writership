b934cd945fbb6d50756700cf490cb67884d7b0c2
phucvin <phucvin2@gmail.com>
2018-09-07 09:29:08 +0700

    start unit test

---
+++ Assets/Tests/Counter.cs
@@ -0,0 +1,24 @@
+﻿using Examples.Counter;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+using Writership;
+
+public class Counter
+{
+    [Test]
+    public void Simple()
+    {
+        var value = Substitute.For<IEl<int>>();
+        var inc = Substitute.For<IOp<Empty>>();
+        var dec = Substitute.For<IOp<Empty>>();
+
+        value.Read().Returns(0);
+        inc.Read().Returns(new List<Empty> { default(Empty) });
+        dec.Read().Returns(new List<Empty>());
+
+        Computers.Value(value, inc, dec);
+
+        value.Received().Write(1);
+    }
+}
