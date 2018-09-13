f6d89fac71bc2f64a5d178614eb63678af1d3184
phucvin <phucvin2@gmail.com>
2018-09-13 10:56:24 +0700

    LiWatcher

---
+++ Assets/Tests/Others.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using Writership;
+
+public class Others
+{
+    private class Item
+    {
+        public readonly IEl<int> Value;
+
+        public Item(IEngine engine, int value)
+        {
+            Value = engine.El(value);
+        }
+    }
+
+    [Test]
+    public void LiOpSimple()
+    {
+        var cd = new CompositeDisposable();
+        var engine = new SinglethreadEngine();
+        var li = engine.Li(new List<Item>());
+        var liOp = new LiWatcher<Item>(cd, engine, li, it => it.Value);
+        var op1 = engine.Op<Empty>();
+        var op2 = engine.Op<Empty>();
+
+        int total = 0;
+        cd.Add(engine.RegisterComputer(
+            new object[] { liOp },
+            () =>
+            {
+                var l = li.Read();
+                total = 0;
+                for (int i = 0, n = l.Count; i < n; ++i)
+                {
+                    total += l[i].Value.Read();
+                }
+            }
+        ));
+
+        cd.Add(engine.RegisterComputer(
+            new object[] { op1, op2 },
+            () =>
+            {
+                if (op1.Read().Count > 0)
+                {
+                    var l = li.AsWrite();
+                    l.Add(new Item(engine, 3));
+                    l.Add(new Item(engine, 2));
+                }
+                if (op2.Read().Count > 0)
+                {
+                    var l = li.Read();
+                    for (int i = 0, n = l.Count; i < n; ++i)
+                    {
+                        l[i].Value.Write(l[i].Value.Read() + 1);
+                    }
+                }
+            }
+        ));
+
+        engine.Update();
+        Assert.AreEqual(0, total);
+        op1.Fire(Empty.Instance);
+        engine.Update();
+        Assert.AreEqual(5, total);
+        op2.Fire(Empty.Instance);
+        engine.Update();
+        Assert.AreEqual(7, total);
+
+        engine.Dispose();
+    }
+}
---
+++ Assets/Writership/LiWatcher.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Writership
+{
+    public class LiWatcher<T> : IOp<Empty>, IHaveCells
+    {
+        private readonly IOp<Empty> inner;
+
+        public IOp<Empty> Applied { get; private set; }
+
+        public LiWatcher(CompositeDisposable cd, IEngine engine, ILi<T> li, Func<T, object> extract)
+        {
+            inner = engine.Op<Empty>();
+            Applied = null;
+
+            IDisposable last = null;
+            cd.Add(engine.RegisterComputer(
+                new object[] { li },
+                () =>
+                {
+                    inner.Fire(Empty.Instance);
+                    engine.MarkDirty(this);
+
+                    var l = li.Read();
+                    var targets = new List<object>();
+                    for (int i = 0, n = l.Count; i < n; ++i)
+                    {
+                        targets.Add(extract(l[i]));
+                    }
+                    if (last != null)
+                    {
+                        last.Dispose();
+                        last = null;
+                    }
+                    if (targets.Count > 0)
+                    {
+                        last = engine.RegisterComputer(
+                            targets.ToArray(),
+                            () =>
+                            {
+                                inner.Fire(Empty.Instance);
+                                engine.MarkDirty(this);
+                            }
+                        );
+                    }
+                }
+            ));
+        }
+
+        public void Fire(Empty value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Empty> Read()
+        {
+            return inner.Read();
+        }
+
+        public void CopyCell(int from, int to)
+        {
+            // Ignore
+        }
+
+        public void ClearCell(int at)
+        {
+            // Ingore
+        }
+    }
+}
