66a0c88f0179f8a5c1235d755ccad76dc5691771
phucvin <phucvin2@gmail.com>
2018-09-21 08:45:38 +0700

    .

--- Assets/Examples/Http/State.cs
+++ Assets/Examples/Http/State.cs
@@ -5,13 +5,17 @@ namespace Examples.Http
 {
     public class State
     {
+        public readonly El<string> UserName;
+        public readonly El<int?> UserId;
         public readonly HttpOp<string, int> HttpUserId;
 
         public State(IEngine engine)
         {
+            UserName = engine.El(string.Empty);
+            UserId = engine.El<int?>(null);
             HttpUserId = new HttpOp<string, int>(engine,
                 "https://api.github.com/users/__USER_NAME__",
-                isSingle: false, allowWriters: true
+                isSingle: true
             ).WithUrlTransformer((url, name) => url.Replace("__USER_NAME__", name)
             ).WithResponseParser(json => JsonUtility.FromJson<GitHubUser>(json).id
             );
@@ -20,6 +24,19 @@ namespace Examples.Http
         public void Setup(CompositeDisposable cd, IEngine engine)
         {
             HttpUserId.Setup(cd, engine);
+
+            engine.Computer(cd, Dep.On(UserName, HttpUserId.Request, HttpUserId.Error, HttpUserId.Response), () =>
+            {
+                if (string.IsNullOrEmpty(UserName)) UserId.Write(null);
+                else if (HttpUserId.Response) UserId.Write(HttpUserId.Response.First);
+                else if (HttpUserId.Error) UserId.Write(null);
+                else if (HttpUserId.Request) UserId.Write(null);
+            });
+
+            engine.Computer(cd, Dep.On(UserName), () =>
+            {
+                if (!string.IsNullOrEmpty(UserName)) HttpUserId.Request.Fire(UserName);
+            });
         }
 
         private struct GitHubUser
